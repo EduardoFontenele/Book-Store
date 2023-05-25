43b6b5da74f2d9b8248da24eb33367b8ec8a11a9
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2023-05-25 01:00:38 -0300

    And I put it in the wrong place

--- src/main/java/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring6api.entities.Author;
-
-public interface AuthorRepository extends JpaRepository<Integer, Author> {
-}
