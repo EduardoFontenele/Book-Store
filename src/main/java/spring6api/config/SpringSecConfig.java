a0aa0c8bf033e3d3c64ff2c3b350e7a2af754d9a
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2023-06-12 18:23:08 -0300

    This branch contains insecure (deprecated) configurations of JWT token and OAuth2. Must not be merged

--- src/main/java/spring6api/config/SpringSecConfig.java
+++ src/main/java/spring6api/config/SpringSecConfig.java
@@ -2,8 +2,10 @@ package spring6api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +14,13 @@ public class SpringSecConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable).build();
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
 }
